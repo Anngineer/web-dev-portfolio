b764e9838484acc4c2b8b4ea1e180dbffa0582e1
Anngineer <anngineerdev@gmail.com>
2025-02-01 00:08:40 -0500

    Update README

--- README.md
+++ README.md
@@ -8,6 +8,4 @@ This repository contains summaries and links to my current web development proje
 
 This project was built with React components in VSCode using HTML, SCSS/CSS, vanilla JavaScript and JSX. Additional packages used include React Router Dom, React Media, and React Scroll. Wireframes and mock-ups for the design were originally sketched using InVision Studio. The images are licensed from Envato Elements and edited using Affinity Designer, and the icons are free use from Font Awesome.
 
-## Contact
 
-Please feel free to contact me at [LinkedIn](www.linkedin.com/in/anngineer) in regard to my work or future projects.
--- src/Contact.js
+++ src/Contact.js
@@ -8,16 +8,15 @@ const Contact = () => {
         <p>Follow my latest projects and professional journey.</p>
 
         <div className="link-box">
-          <a href={"https://anngineer.substack.com/"}>
-            <i className="fas fa-blog" alt="blog icon"></i>
           
-           Blog
-          </a>
           <a href={"https://github.com/Anngineer"}>
             <i className="fab fa-github" alt="GitHub icon"></i> GitHub
           </a>
           <a href={"https://www.linkedin.com/in/anngineer/"}>
             <i className="fab fa-linkedin" alt="Linked In Icon"></i> LinkedIn
+          </a>
+          <a href={"https://anngineer.substack.com/"}>
+            <i className="fas fa-blog" alt="blog icon"></i> Blog
           </a>
         </div>
 
